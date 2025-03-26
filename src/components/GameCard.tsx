5dc4db222c57633149912dc53f0142c61f39099e
Gabriele Giunchi <gabriele.giunchi1994@gmail.com>
2025-03-26 21:01:33 +0100

    Update GameCard.tsx

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -24,7 +24,7 @@ export function GameCard({
     if (isCorrect) {
       const timer = setTimeout(() => {
         onNextWord();
-      }, 5);
+      }, 100);
       return () => clearTimeout(timer);
     }
   }, [isCorrect, onNextWord]);

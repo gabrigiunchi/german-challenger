e27c080e9c85eaa50f24123c533ebceec12390b4
Gabriele Giunchi <gabriele.giunchi1994@gmail.com>
2025-10-26 10:20:07 +0100

    remove router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import {Routes, Route, Navigate, MemoryRouter} from 'react-router-dom';
 import {Game} from './pages/Game';
 
 function App() {
   return (
-    <MemoryRouter>
-      <Routes>
-        <Route path="/game" element={<Game />} />
-        <Route path="*" element={<Navigate to="/game" replace />} />
-      </Routes>
-    </MemoryRouter>
+    <Game />
   );
 }
 
